7c3557604ad87ce53e107d4acf3dbe77788cb8d2
liuhuijieweb <16619970085@163.com>
2018-05-09 17:01:26 +0800

    step

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <template>
-  <cut-img></cut-img>
+  <div id="app">
+    <img src="./assets/logo.png">
+
+    <!--<router-view/>-->
+  </div>
 </template>
 
 <script>
-  import CutImg from './components/CutImg.vue'
-
 export default {
-  name: 'app',
-  components:{
-    CutImg
-  },
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+  name: 'App'
 }
 </script>
 
@@ -26,23 +20,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
 }
 </style>
